5271ee9e64f60a50e437a872be85b34f8551c88b
Arya Umesh <74286505+Python-is-better-than-Java@users.noreply.github.com>
2023-11-24 00:12:18 +0530

    Added signup and login pages

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ import webbrowser
 import mysql.connector
 from Game import gameplay
 pygame.init()
-screen = pygame.display.set_mode((1000, 800))
+screen = pygame.display.set_mode((1000, 770))
 pygame.display.set_caption("PRAAJEQT")
 
 # Create a connection object
@@ -49,8 +49,8 @@ def login_page():
     password_hash = ''
     enter_again = ''
     key = 0 # To change from entering username to entering password
-    player_rect = pygame.Rect(200, 120, 600, 60)
-    password_rect = pygame.Rect(200, 320, 600, 60)
+    player_rect = pygame.Rect(200, 90, 600, 60)
+    password_rect = pygame.Rect(200, 240, 600, 60)
     colour = pygame.Color("Purple")
     while True:
         login_mouse_pos = pygame.mouse.get_pos()
@@ -61,7 +61,7 @@ def login_page():
         screen.blit(PLAY_TEXT, PLAY_RECT)
 
         PASSWORD_TEXT = SysFont("Calibri", 45).render("Enter password", True, "White")
-        PASSWORD_RECT = PASSWORD_TEXT.get_rect(center=(500, 250))
+        PASSWORD_RECT = PASSWORD_TEXT.get_rect(center=(500, 200))
         screen.blit(PASSWORD_TEXT, PASSWORD_RECT)
 
         AGAIN_TEXT = SysFont("Calibri", 30).render(enter_again, True, "White")
@@ -76,11 +76,11 @@ def login_page():
         text_password_hash = SysFont("Calibri", 50).render(password_hash, True, (255, 255, 255)) 
         screen.blit(text_password_hash, (password_rect.x+5, password_rect.y+5))
 
-        START_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 450), 
+        START_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 400), 
                             text_input="Start", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
         START_BUTTON.changeColor(login_mouse_pos)
         START_BUTTON.update(screen)
-        PLAY_BACK = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 600), 
+        PLAY_BACK = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 550), 
                            text_input="Back", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
         PLAY_BACK.changeColor(login_mouse_pos)
         PLAY_BACK.update(screen)
@@ -125,6 +125,92 @@ def login_page():
 
         pygame.display.flip()
 
+def signup_page():
+    player_name = ''
+    password = ''
+    pswd_prev_len = len(password)
+    password_hash = ''
+    enter_again = ''
+    key = 0 # To change from entering username to entering password
+    player_rect = pygame.Rect(200, 90, 600, 60)
+    password_rect = pygame.Rect(200, 240, 600, 60)
+    colour = pygame.Color("Purple")
+    while True:
+        signup_mouse_pos = pygame.mouse.get_pos()
+        screen.fill((125, 0, 0))
+
+        PLAY_TEXT = SysFont("Calibri", 45).render("Enter username of the player", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(500, 50))
+        screen.blit(PLAY_TEXT, PLAY_RECT)
+
+        PASSWORD_TEXT = SysFont("Calibri", 45).render("Set a password", True, "White")
+        PASSWORD_RECT = PASSWORD_TEXT.get_rect(center=(500, 200))
+        screen.blit(PASSWORD_TEXT, PASSWORD_RECT)
+
+        AGAIN_TEXT = SysFont("Calibri", 30).render(enter_again, True, "White")
+        AGAIN_RECT = PLAY_TEXT.get_rect(center=(500, 700))
+        screen.blit(AGAIN_TEXT, AGAIN_RECT)
+
+        pygame.draw.rect(screen, colour, player_rect)
+        text_player_name = SysFont("Calibri", 50).render(player_name, True, (255, 255, 255)) 
+        screen.blit(text_player_name, (player_rect.x+5, player_rect.y+5))
+
+        pygame.draw.rect(screen, colour, password_rect)
+        text_password_hash = SysFont("Calibri", 50).render(password_hash, True, (255, 255, 255)) 
+        screen.blit(text_password_hash, (password_rect.x+5, password_rect.y+5))
+
+        DONE_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 400), 
+                            text_input="Done", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
+        DONE_BUTTON.changeColor(signup_mouse_pos)
+        DONE_BUTTON.update(screen)
+        PLAY_BACK = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 550), 
+                           text_input="Back", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
+        PLAY_BACK.changeColor(signup_mouse_pos)
+        PLAY_BACK.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+      
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(signup_mouse_pos):
+                    home_screen()
+                if DONE_BUTTON.checkForInput(signup_mouse_pos):
+                    # check if user exists in the player_profile table
+                    query = f"SELECT Username FROM player_profile WHERE Username = '{player_name}';"
+                    cursor_new.execute(query)
+                    results = cursor_new.fetchall()
+                    if (len(results) == 0):
+                        insert_query = f"INSERT INTO player_profile VALUES('{player_name}', '{password}');"
+                        cursor_new.execute(insert_query)
+                        conn_new.commit()
+                        home_screen()
+                    else:
+                        enter_again = "Username already exists. Try Again."
+                        key = 0 # Reset to entering username
+
+                    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    if (key == 0):
+                        player_name = player_name[:-1]
+                    else:
+                        password = password[:-1]
+                        password_hash = password_hash[:-1]
+                        pswd_prev_len = len(password)
+                elif event.key == pygame.K_RETURN:
+                    key = 1  # Username entered, now enter password
+                else:
+                    if (key == 0):
+                        player_name += event.unicode # Unicode standard is used for string formation
+                    else:
+                        password += event.unicode
+                        if (len(password) == pswd_prev_len + 1):
+                            password_hash += '*'
+                            pswd_prev_len = len(password)
+
+        pygame.display.flip()
+
 def home_screen():
     while True:
         screen.fill((125, 0, 0))
@@ -132,18 +218,20 @@ def home_screen():
         home_mouse_pos = pygame.mouse.get_pos()
 
         home_text = SysFont("Calibri", 70).render("Start Game", True, "White")
-        home_button = home_text.get_rect(center=(500, 100))
+        home_button = home_text.get_rect(center=(500, 50))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 200), 
                             text_input="Play", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
-        TABLE_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 400), 
+        TABLE_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 350), 
                             text_input="Display Tables", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
-        QUIT_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 550), 
+        CREATE_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 500),
+                            text_input="Create Player", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
+        QUIT_BUTTON = Button(image=pygame.image.load("Rectangle.png"), pos=(500, 650), 
                             text_input="Quit", font=SysFont("Calibri", 70), base_color="#d7fcd4", hovering_color="Green")
 
         screen.blit(home_text, home_button)
 
-        for button in [PLAY_BUTTON, TABLE_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, TABLE_BUTTON, CREATE_BUTTON, QUIT_BUTTON]:
             button.changeColor(home_mouse_pos)
             button.update(screen)
         
@@ -155,6 +243,8 @@ def home_screen():
                     login_page()
                 if TABLE_BUTTON.checkForInput(home_mouse_pos):
                     webbrowser.open("http://localhost:8501")
+                if CREATE_BUTTON.checkForInput(home_mouse_pos):
+                    signup_page()
                 if QUIT_BUTTON.checkForInput(home_mouse_pos):
                     pygame.quit()
 
